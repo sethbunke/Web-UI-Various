8d435f5a8718787aae8de7023a51ca62181f34f4
sethbunke <sjb_strat@hotmail.com>
2018-06-06 16:38:08 -0700

    racing working with responsive

--- layouts/racing/js/main.js
+++ layouts/racing/js/main.js
@@ -1,5 +1,12 @@
 $(document).ready(function(){
 
-    $('.hamburger-menu').click(function(){    $('main').addClass('active');  });
+    $('.hamburger-menu').click(function(event){
+        event.stopPropagation();
+        if ($('main').hasClass('active')){
+          $('main').removeClass('active');
+        } else {
+          $('main').addClass('active');
+        }
+      });
   
   });
